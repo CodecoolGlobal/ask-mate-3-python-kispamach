1b3ebda4c4eaa358657989ac58cc65ad534f1702
haku9104 <horvathbalazs9104@gmail.com>
2021-01-19 15:46:09 +0100

    01.19 end of day state

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 import data_handler
 import util
 import engine
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_folder="images")
 
@@ -20,7 +21,30 @@ def main_page():
 def question_page(id=None):
     question_list = data_handler.reader('question')
     question = list(filter(lambda record: record["id"] == id, question_list))[0]
-    return render_template("question.html", question=question)
+    answer_list = data_handler.reader('answer')
+    answers = list(filter(lambda record: record["question_id"] == id, answer_list))
+    return render_template("question.html", question=question, answers=answers)
+
+
+@app.route("/add-question", methods=["GET", "POST"])
+def add_question():
+    if request.method == "POST":
+        f = request.files['picture_upload']
+        # extension = f.content_type.split("/")
+        # print(extension)
+        f.save("./images/uploaded_images/" + secure_filename("dicpic." + f.content_type.split("/")[1]))
+        return 'file uploaded successfully'
+        # return redirect("/")
+    return render_template("add_question.html")
+
+
+@app.route("/answer/<answer_id>/delete")
+def delete_answer(answer_id=None):
+    answer_list = data_handler.reader('answer')
+    answers = list(filter(lambda record: record["id"] != answer_id, answer_list))
+    question_id = list(filter(lambda record: record["id"] == answer_id, answer_list))[0]["question_id"]
+    data_handler.writer("answer", answers)
+    return redirect("/question/" + question_id)
 
 
 @app.route("/question/<id>/upvote")
