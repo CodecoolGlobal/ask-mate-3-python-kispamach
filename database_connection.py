20f1b515105042c97447acab1886fb0d1e98ee2f
Gergo <ferenczi.gergo.87@gmail.com>
2021-02-06 19:37:06 +0100

    Setup heroku

--- database_connection.py
+++ database_connection.py
@@ -4,6 +4,17 @@ import os
 
 import psycopg2
 import psycopg2.extras
+import urllib
+
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+connection = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 
 def get_connection_string():
@@ -31,7 +42,7 @@ def get_connection_string():
 def open_database():
     try:
         connection_string = get_connection_string()
-        connection = psycopg2.connect(connection_string)
+        # connection = psycopg2.connect(connection_string)
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
