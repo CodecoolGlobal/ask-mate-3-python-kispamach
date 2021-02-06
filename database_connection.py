72715ad9f0f6295e275309467028ada3e581ae4d
Gergo <ferenczi.gergo.87@gmail.com>
2021-02-06 19:56:59 +0100

    Fix datatabase error v2.0

--- database_connection.py
+++ database_connection.py
@@ -6,25 +6,20 @@ import psycopg2
 import psycopg2.extras
 import urllib
 
-urllib.parse.uses_netloc.append('postgres')
-url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-connection = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
-
 
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
-
+    # user_name = os.environ.get('PSQL_USER_NAME')
+    # password = os.environ.get('PSQL_PASSWORD')
+    # host = os.environ.get('PSQL_HOST')
+    # database_name = os.environ.get('PSQL_DB_NAME')
+    urllib.parse.uses_netloc.append('postgres')
+    url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+    user_name=url.username,
+    password=url.password,
+    host=url.hostname + ":" + url.port,
+    database_name=url.path[1:]
     env_variables_defined = user_name and password and host and database_name
 
     if env_variables_defined:
@@ -52,7 +47,7 @@ def open_database():
 
 def connection_handler(function):
     def wrapper(*args, **kwargs):
-        # connection = open_database()
+        connection = open_database()
         # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provide dictionaries)
         dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         ret_value = function(dict_cur, *args, **kwargs)
