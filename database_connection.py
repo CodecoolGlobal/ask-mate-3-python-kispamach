46f23fdb4fb6ea1340b527bc7dcb021767bbb66f
Gergo <ferenczi.gergo.87@gmail.com>
2021-02-06 20:02:37 +0100

    Fix datatabase error v2.2

--- database_connection.py
+++ database_connection.py
@@ -18,7 +18,8 @@ def get_connection_string():
     url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
     user_name=url.username,
     password=url.password,
-    host=url.hostname + ":" + str(url.port),
+    host=url.hostname,
+    # host=url.hostname + ":" + str(url.port),
     database_name=url.path[1:]
     env_variables_defined = user_name and password and host and database_name
 
